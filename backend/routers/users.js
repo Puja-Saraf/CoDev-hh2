e237bb2200c5e1181bab1a9615915227f3db5b84
jalanprakhar <iit2020200@iiita.ac.in>
2022-06-25 21:06:24 +0530

    implemented functionalities to match,reject & unreject users and also fetch resepective user arrays

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -8,4 +8,14 @@ userRouter.get("/users", userController.getAllUsers);
 
 userRouter.put("/user", userController.updateUser);
 
+userRouter.get("/matched", userController.getMatchedUsers);
+
+userRouter.get("/pending", userController.getPendingUsers);
+
+userRouter.put("/match", userController.postMatchUser);
+
+userRouter.put("/reject", userController.rejectUser);
+
+userRouter.put("/unreject", userController.unrejectUser);
+
 module.exports = userRouter;
