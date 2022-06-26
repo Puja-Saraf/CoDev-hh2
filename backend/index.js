515c0aaeeabfafdf8d3b9b069294b1de733fc62e
jalanprakhar <iit2020200@iiita.ac.in>
2022-06-26 09:55:36 +0530

    Added message model and also implemented the get and post message functionality in the backend

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ require("./db/connection.js");
 const authRouter = require("./routers/auth.js");
 const userRouter = require("./routers/users.js");
 const app = express();
+const messageRouter = require("./routers/messages.js");
 
 app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
+app.use("/messages", messageRouter);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
